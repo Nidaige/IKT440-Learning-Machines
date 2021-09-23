408058abcfbd672fe0698df030b20ad89903be85
Henning Blomfeldt Thorsen <henky95@gmail.com>
2021-09-23 16:39:38 +0200

    Started part 2  of assignment 2 - implemented type 1 and 2 feedback and memory
    Only missing function is generating several rules and then using them to predict a patient

--- assignment2.py
+++ assignment2.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 
@@ -13,36 +14,7 @@ def evaluate_condition(observation, condition):     # copied from https://github
     return truth_value_of_condition
 
 
-class Memory:
-    def __init__(self, forget_value, memorize_value, memory):
-        self.memory = memory
-        self.forget_value = forget_value
-        self.memorize_value = memorize_value
 
-    def get_memory(self):
-        return self.memory
-
-    def get_literals(self):
-        return list(self.memory.keys())
-
-    def get_condition(self):
-        condition = []
-        for literal in self.memory:
-            if self.memory[literal] >= 6:
-                condition.append(literal)
-        return condition
-
-    def memorize(self, literal):
-        if random.random() <= self.memorize_value and self.memory[literal] < 10:
-            self.memory[literal] += 1
-
-    def forget(self, literal):
-        if random.random() <= self.forget_value and self.memory[literal] > 1:
-            self.memory[literal] -= 1
-
-    def memorize_always(self, literal):
-        if self.memory[literal] < 10:
-            self.memory[literal] += 1
 
 
 # Evaluating rules part ------------------------------------------------------------------
@@ -107,3 +79,109 @@ for a in range(len(patientsRecurrence)):
 
 
 # Type 1 and 2 feedback part --------------------------------------------------------------------------
+
+MemorySize = 50000 # The total range of how forgotten/remembered a given feature is
+MemoryInitVal = math.floor(MemorySize*0.5) # The initial value of every feature in memory. Half of the memory size
+MemoryIterations = 5 # How many remember/forget iterations will be done with the memory before extracting a rule?
+
+class Memory:
+    def __init__(self, forget_value, memorize_value, memory):
+        self.memory = memory
+        self.forget_value = forget_value
+        self.memorize_value = memorize_value
+
+    def get_memory(self):
+        return self.memory
+
+    def get_literals(self):
+        return list(self.memory.keys())
+
+    def get_condition(self):
+        condition = []
+        for literal in self.memory:
+            if self.memory[literal] >= MemoryInitVal:
+                condition.append(literal)
+        return condition
+
+    def memorize(self, literal):
+        if random.random() <= self.memorize_value and self.memory[literal] < MemorySize:
+            self.memory[literal] += 1
+
+    def forget(self, literal):
+        if random.random() <= self.forget_value and self.memory[literal] > 1:
+            self.memory[literal] -= 1
+
+    def memorize_always(self, literal):
+        if self.memory[literal] < MemorySize:
+            self.memory[literal] += 1
+
+def type_i_feedback(observation, memory): # function that gives type i feedback on a memory:
+    remaining_literals = memory.get_literals() # gets all the literals (features)
+    if evaluate_condition(observation, memory.get_condition()) == True:
+        for feature in observation: # iterate through features in observation
+            if observation[feature] == True: # if the feature exists
+                memory.memorize(feature) # memorize the feature
+                remaining_literals.remove(feature)
+            elif observation[feature] == False: # if the feature does not exist in the observation
+                memory.memorize('NOT ' + feature) # memorize the negative (NOT) feature instead
+                remaining_literals.remove('NOT ' + feature)
+    for literal in remaining_literals: # for any non-memorized feature,
+        memory.forget(literal) # forget about it
+
+
+def type_ii_feedback(observation, memory):
+    if evaluate_condition(observation, memory.get_condition()) == True:
+        for feature in observation: # for any feature in the observation,
+            if observation[feature] == False: # if the feature exists,
+                memory.memorize_always(feature) # memorize the feature
+            elif observation[feature] == True: # if the feature does not exist:
+                memory.memorize_always('NOT ' + feature) # memorize the negative (NOT) feature
+
+def classify(observation, recurrence_rules, non_recurrence_rules):
+    vote_sum = 0
+    for rule in recurrence_rules:
+        if evaluate_condition(observation, rule.get_condition()) == True:
+            vote_sum += 1
+    for rule in non_recurrence_rules:
+        if evaluate_condition(observation, rule.get_condition()) == True:
+            vote_sum -= 1
+    if vote_sum >= 0:
+        return "Recurrence"
+    else:
+        return "Non-recurrence"
+
+Recurring_Patients = [
+{Menopge40:True,  Menoplt40:False,Menopremeno:False, Inv02:False, Inv35:True,  Inv68:False,  DegMalg3:True,  DegMalg2:False, DegMalg1:False},
+{Menopge40:True,  Menoplt40:False,Menopremeno:False, Inv02:False, Inv35:False, Inv68:True,   DegMalg3:True,  DegMalg2:False, DegMalg1:False},
+{Menopge40:False, Menoplt40:False,Menopremeno:True,  Inv02:True,  Inv35:False, Inv68:False,  DegMalg3:True,  DegMalg2:False, DegMalg1:False},
+]
+
+Non_Recurring_Patients = [
+{Menopge40:False, Menoplt40:True, Menopremeno:False, Inv02:True,  Inv35:False, Inv68:False,  DegMalg3:True,  DegMalg2:False, DegMalg1:False},
+{Menopge40:True,  Menoplt40:False,Menopremeno:False, Inv02:True,  Inv35:False, Inv68:False,  DegMalg3:False, DegMalg2:True,  DegMalg1:False},
+{Menopge40:False, Menoplt40:False,Menopremeno:True,  Inv02:True,  Inv35:False, Inv68:False,  DegMalg3:False, DegMalg2:False, DegMalg1:True},
+]
+
+# All features, starting value of 5, goes from 0 to 10, where 0 is most forgotten, 10 is most remembered
+RecurranceAllFeatures = {Menopge40: MemoryInitVal,NotMenopge40: MemoryInitVal, Menoplt40: MemoryInitVal, NotMenoplt40: MemoryInitVal, Menopremeno: MemoryInitVal, NotMenopremeno: MemoryInitVal, Inv02: MemoryInitVal, NotInv02: MemoryInitVal, Inv35: MemoryInitVal, NotInv35: MemoryInitVal, Inv68: MemoryInitVal, NotInv68: MemoryInitVal, DegMalg3: MemoryInitVal, NotDegMalg3: MemoryInitVal, DegMalg2: MemoryInitVal, NotDegMalg2: MemoryInitVal, DegMalg1: MemoryInitVal, NotDegMalg1: MemoryInitVal}
+
+Recurring_Rule= Memory(0.8,0.2,RecurranceAllFeatures) # initialize memory with forget-value 0.8, remember-value 0.2, and the above memory.
+Non_Recurring_Rule= Memory(0.8,0.2,RecurranceAllFeatures) # initialize memory with forget-value 0.8, remember-value 0.2, and the above memory.
+for a in range(MemoryIterations): # do 100 runs
+    # Recurring memory learns
+    id = random.choice([0,1,2]) # randomly select a patient id
+    type = random.choice([0,1]) # randomly choose feedback type
+    if (type==1):
+        type_i_feedback(Recurring_Patients[id], Recurring_Rule) # feedback type i
+    else:
+        type_ii_feedback(Recurring_Patients[id], Recurring_Rule) # feedback type ii
+    # Non-recurring memory learns
+    id = random.choice([0,1,2]) # randomly select a patient id
+    type = random.choice([0,1]) # randomly choose feedback type
+    if (type==1):
+        type_i_feedback(Non_Recurring_Patients[id], Non_Recurring_Rule) # feedback type i
+    else:
+        type_ii_feedback(Non_Recurring_Patients[id], Non_Recurring_Rule) # feedback type ii
+
+for patient in patients:
+    print(classify(patient, [Recurring_Rule], [Non_Recurring_Rule]))
