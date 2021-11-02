731749c3102c7f9b8ae92dabfd56a4d69f978a6c
Nidaige <henky95@gmail.com>
2021-11-02 10:20:39 +0100

    Created project file, added some notes

---
+++ project - go.py
@@ -0,0 +1,11 @@
+'''
+Project description:
+- Go boards: parse data, predict a winner
+- Same as assignment 3, but more difficult dataset.
+Steps:
+1. Download data
+2. Build data loader
+3. Re-use code from assignment 3, but this time initialize with bools (see uploaded example on canvas)
+4. Train the machine
+5. Evaluate
+'''
