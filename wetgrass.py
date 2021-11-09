f9d9eaa3f8f239a51e3965168d1604b9fb491d0b
Henning Blomfeldt Thorsen <henky95@gmail.com>
2021-11-09 17:25:10 +0100

    Fixed simulations etc.

--- wetgrass.py
+++ wetgrass.py
@@ -13,52 +13,55 @@ Dictionary approact: node.prob["w1w2w3...wn"]
     - Manually adding each probability
 '''
 from random import random, choice
-
 # Rain is represented by R
 # Sprinkler is represented by S
 # Watson is represented by W
 # Holmes is represented by H
 # Negations are represented by a leading N
+#   Not rain = NR,       Not Sprinkler = NS,    etc.
 
 RP = 155/366 # probability of rain. 155 days of rain in 2020 (leap year) = 155/366
 SP = (3*16*7)/365   # probability of sprinkler on: 3 hours * 7 days/week * 4 weeks/month * 4 months/year
-WP = {"NRNS": 0.0001, "NRS": 0.0001,  "RNS": 0.999, "RS": 0.999}  # probability of wilson given rain.
+WP = {"NRNS": 0.0001, "NRS": 0.0001,  "RNS": 0.999, "RS": 0.999}  # probability of wilson given rain and sprinkler. (Probability does not change with sprinkler, as it is independent)
 HP = {"NRNS": 0.0001, "NRS": 0.8,     "RNS": 0.999, "RS": 0.9999999}  # probability of holmes given rain and sprinkler
 RS = [["NRNS","NRS"],["RNS","RS"]]  # list of state combinations
-MBs = {"R": "WHS" ,"S": "HR" ,"H": "RS", "W": "R"} # dict of markov blankets for each node
-formulas = {"R": "alpha*RP*HP[RS[mb_activations[\"R\"]][mb_activations[\"S\"]]*WP[RS[mb_activations[\"R\"]][mb_activations[\"S\"]]]", # p(rain|world) = alpha * p(rain)*p(wilson|rain)*p(holmes|rain,sprinkler)
-            "S": "alpha*SP*HP[RS[mb_activations[\"R\"]][mb_activations[\"S\"]]*WP[RS[mb_activations[\"R\"]][mb_activations[\"S\"]]]", # p(sprinkler|world) = alpha * P(sprinkler)*p(wilson|rain)*p(holmes|rain, sprinkler)
-            "W": "alpha*WP[RS[mb_activations[\"R\"]][mb_activations[\"S\"]]]", # p(W|world) = alpha * p(W|Rain,Sprinkler) = p(W|rain)
-            "H": "alpha*HP[RS[mb_activations[\"R\"]][mb_activations[\"S\"]]]" # p(W|world) = alpha * p(W|Rain,Sprinkler) = p(W|rain)
+MBs = {"R": "WHSR", "S": "WHSR", "H": "WHSR", "W": "WHSR", } # dict of markov blankets for each node
+formulas = {"R": "alpha * RP * HP[RS[mb_activations[\"R\"]][mb_activations[\"S\"]]] * WP[RS[mb_activations[\"R\"]][mb_activations[\"S\"]]]",
+            "S": "alpha * SP * HP[RS[mb_activations[\"R\"]][mb_activations[\"S\"]]]",
+            "W": "alpha * WP[RS[mb_activations[\"R\"]][mb_activations[\"S\"]]]",
+            "H": "alpha * HP[RS[mb_activations[\"R\"]][mb_activations[\"S\"]]]",
              }
-
-
 # Set states of observed nodes to observations, and all unobserved nodes to random
-R = int(random() > RP)  # set state of rain given p(rain)
-S = int(random() > SP)  # set state of sprinkler given p(sprinkler)
-H = int(random() > HP[RS[R][S]])  # set state of Holmes given rain and sprinkler
-W = int(random() > WP[RS[R][S]])  # set state of Watson given rain (value does not depend on sprinkler)
-alpha = 1.0
-for a in range(10):
-    current = choice(["R","S","H","W"]) #  Randomly select a node
-    print("current",current)
-    mb = MBs[current] # Get its markov blanket
-    # get the states in markov blanket
-    mb_activations = {}
-    for node in mb:
-        mb_activations[node] = eval(node)
-    print("markov blanket with states:",mb_activations)
-    form = (formulas[current])
-    print(form)
-    print(str(alpha * RP * HP[RS[mb_activations["R"]][mb_activations["S"]]] * WP[RS[mb_activations["R"][mb_activations["S"]]]]))
-    # no parents case:
-    # p(rain | W_rain) = a . P(rain) . P(W|rain) . P(h|rain,sprinkler)
-    # now calculate p(node=state| markov blanket) for each state (0 and 1)
-
-
-
-
-
-
-
-
+activations = {"R": 0, "S": 0, "H": 0, "W": 0}
+totals = {"R": 0, "S": 0, "H": 0, "W": 0}
+names = {"R": "It rains", "S": "The sprinkler is on", "H": "Holmes' grass is wet", "W": "Watson's grass is wet"}
+for runs in range(1000): # for 1000 runs of the algorithm (since subsequent probabilities are very dependent on the first set, producing either close to 0 or close to 1)
+    R = int(random() < RP)  # set state of rain given p(rain)
+    S = int(random() < SP)  # set state of sprinkler given p(sprinkler)
+    H = int(random() < HP[RS[R][S]])  # set state of Holmes given the current rain and sprinkler
+    W = int(random() < WP[RS[R][S]])  # set state of Watson given the current rain
+    # trying with "true random" activation, not caring about probabilities
+    states = {"R":R, "S":S, "H": H, "W":W}
+    alpha = 1.0 # alpha value. 
+    for a in range(500):
+        current = choice(["R","S","H","W"]) #  Randomly select a node
+        mb_activations = {}  # get activation of each node (which are "on" i.e. is it raining, is the sprinkler on, wet grass)
+        for node in "WHRS":
+            mb_activations[node] = states[node]
+        # get probability of <node> activating using formula
+        # formula for each node only uses the markov blanket, so there is no need to explicitly get and use it here
+        form = (formulas[current])
+        new_probability = eval(form)
+        new_activation = int(random() < new_probability)
+        #print("New activation",new_activation)
+        totals[current]+=1
+        activations[current]+=new_activation
+        #print(current,new_activation,new_probability)
+print("Final probabilities:")
+for a in totals.keys():
+    try:
+        print("P(",names[a],") = ",activations[a]/totals[a])
+    except:
+        print(a,"didn't get updated")
+    # set new value/probability of <node> based on new distribution?
+exit()
